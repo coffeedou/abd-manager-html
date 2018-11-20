b2b62b03cb2d4cecf552280a1139b35dfdd91990
liukai <liu1113855@sina.com>
2018-11-20 18:28:58 +0800

    提交更新的代码

--- src/views/wordIndex/index.vue
+++ src/views/wordIndex/index.vue
@@ -1,13 +1,62 @@
 <template>
-    $END$
+    <!--<div class="app-wrapper">-->
+        <el-container>
+            <el-header>
+                <el-input placeholder="请输入内容" v-model="word" clearable> </el-input>
+            </el-header>
+            <el-main>
+                <canvas id="lineChart1" width="60%" height="40%"></canvas>
+                <canvas id="lineChart2" width="60%" height="40%"></canvas>
+            </el-main>
+            <el-footer>Footer</el-footer>
+        </el-container>
+    <!--</div>-->
 </template>
 
 <script>
+    import Chart from 'chart.js';
+
     export default {
-        name: "index"
+        data() {
+            return {
+                word: ''
+            }
+        },
+        // 初始化以后执行
+        mounted() {
+            var myChart = new Chart("lineChart1", {
+                type: 'line',
+                data: {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    datasets: [{
+                        label: 'My First dataset',
+                        backgroundColor: window.chartColors.red,
+                        borderColor: window.chartColors.red,
+                        data: [
+                            // randomScalingFactor(),
+                            // randomScalingFactor(),
+                            // randomScalingFactor(),
+                            // randomScalingFactor(),
+                            // randomScalingFactor(),
+                            // randomScalingFactor(),
+                            // randomScalingFactor()
+                        ],
+                        fill: false,
+                    }]
+                }
+            });
+
+        }
     }
 </script>
 
 <style scoped>
-
+    .el-container {
+        height: 100%;
+    }
+    /*.app-wrapper {*/
+        /*position: relative;*/
+        /*height: 100%;*/
+        /*width: 100%;*/
+    /*}*/
 </style>
