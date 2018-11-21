9850cb0b2e410724eaa2bc7caaeae9e97038707e
liukai <liu1113855@sina.com>
2018-11-21 17:57:27 +0800

    chart.js line 显示

--- src/views/wordIndex/index.vue
+++ src/views/wordIndex/index.vue
@@ -2,11 +2,11 @@
     <!--<div class="app-wrapper">-->
         <el-container>
             <el-header>
-                <el-input placeholder="请输入内容" v-model="word" clearable> </el-input>
+                <!--<el-input placeholder="请输入内容" v-model="word" @keyup.enter="search" clearable> </el-input>-->
             </el-header>
             <el-main>
-                <canvas id="lineChart1" width="60%" height="40%"></canvas>
-                <canvas id="lineChart2" width="60%" height="40%"></canvas>
+                <canvas id="lineChart1" height="50"></canvas>
+                <canvas id="lineChart2" height="50"></canvas>
             </el-main>
             <el-footer>Footer</el-footer>
         </el-container>
@@ -15,36 +15,64 @@
 
 <script>
     import Chart from 'chart.js';
+    import axios from 'axios';
 
     export default {
         data() {
             return {
-                word: ''
+                word: '4g'
             }
         },
         // 初始化以后执行
         mounted() {
-            var myChart = new Chart("lineChart1", {
-                type: 'line',
-                data: {
-                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                    datasets: [{
-                        label: 'My First dataset',
-                        backgroundColor: window.chartColors.red,
-                        borderColor: window.chartColors.red,
-                        data: [
-                            // randomScalingFactor(),
-                            // randomScalingFactor(),
-                            // randomScalingFactor(),
-                            // randomScalingFactor(),
-                            // randomScalingFactor(),
-                            // randomScalingFactor(),
-                            // randomScalingFactor()
-                        ],
-                        fill: false,
-                    }]
-                }
-            });
+            axios.get('http://127.0.0.1:8080/abd/wordfrequency/word/' + this.word)
+                .then(response => {
+                    let result = response.data.data
+                    let labels = Object.keys(result)
+                    let values = Object.values(result);
+
+                    var myChart1 = new Chart("lineChart1", {
+                        type: 'line',
+                        data: {
+                            labels: labels,
+                            datasets: [{
+                                label: 'My First dataset',
+                                backgroundColor: 'rgb(255, 99, 132)',
+                                borderColor: 'rgb(255, 99, 132)',
+                                data: values,
+                                fill: false,
+                            }]
+                        }
+                    });
+                })
+                .catch(error => console.log(error))
+
+            axios.get('http://127.0.0.1:8080/abd/wordfrequency/topic/' + this.word)
+                .then(response => {
+                    let result = response.data.data
+                    let labels = Object.keys(result)
+                    let values = Object.values(result);
+
+                    var myChart2 = new Chart("lineChart2", {
+                        type: 'line',
+                        data: {
+                            labels: labels,
+                            datasets: [{
+                                label: 'My First dataset',
+                                backgroundColor: 'rgb(255, 99, 132)',
+                                borderColor: 'rgb(255, 99, 132)',
+                                data: values,
+                                fill: false,
+                            }]
+                        }
+                    });
+                })
+                .catch(error => console.log(error))
+
+
+        },
+
+        methods: {
 
         }
     }
